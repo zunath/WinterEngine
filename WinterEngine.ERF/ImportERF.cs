5cf9e8eeea8d6b9ae8780e7e7983c89e2c92b6c4
Zunath <coolty3001@yahoo.com>
2013-01-18 21:20:56 -0600

    Started work on the ERF import functionality.

--- WinterEngine.ERF/ImportERF.cs
+++ WinterEngine.ERF/ImportERF.cs
@@ -15,5 +15,52 @@ namespace WinterEngine.ERF
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Handles selecting all items in the list box when user clicks the "Select All" button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonSelectAll_Click(object sender, EventArgs e)
+        {
+            // Disable the visibility so that the control won't try to redraw itself
+            // while selecting all objects. This speeds up the process.
+            listBoxResources.Visible = false;
+
+            for (int index = 0; index < listBoxResources.Items.Count; index++)
+            {
+                listBoxResources.SetSelected(index, true);
+            }
+
+            // Make it visible again.
+            listBoxResources.Visible = true;
+        }
+
+        /// <summary>
+        /// Handles de-selecting all items in the list box when user clicks the "Select None" button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonSelectNone_Click(object sender, EventArgs e)
+        {
+            listBoxResources.Visible = false;
+
+            for (int index = 0; index < listBoxResources.Items.Count; index++)
+            {
+                listBoxResources.SetSelected(index, false);
+            }
+
+            listBoxResources.Visible = true;
+        }
+
+        /// <summary>
+        /// Handles exiting the import ERF form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
     }
 }
