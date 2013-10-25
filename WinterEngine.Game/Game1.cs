cefed199b383296716bde6b042659e9a169c1adb
Zunath <coolty3001@yahoo.com>
2013-10-24 20:34:00 -0500

    Moved window resizing logic from screen to root Game1 class.

--- WinterEngine.Game/Game1.cs
+++ WinterEngine.Game/Game1.cs
@@ -52,6 +52,8 @@ namespace WinterEngine.Game
             form = Form.FromHandle(FlatRedBallServices.WindowHandle) as Form;
             form.MinimumSize = new Size(200, 200);
             form.Resize += HandleResize;
+            //form.ResizeEnd += HandleResize;
+            FlatRedBallServices.CornerGrabbingResize += ReactToResizing;
         }
 
         protected override void Update(GameTime gameTime)
@@ -80,5 +82,59 @@ namespace WinterEngine.Game
                 FlatRedBallServices.UnsuspendEngine();
             }
         }
+
+
+        private void ReactToResizing(object sender, EventArgs e)
+        {
+            // Get the new client bounds (the area where things will be drawn)
+            Microsoft.Xna.Framework.Rectangle displayRectangle =
+                FlatRedBallServices.Game.Window.ClientBounds;
+
+            // This tests if the user has minimized the window
+            if (displayRectangle.Width == 0 || displayRectangle.Height == 0)
+            {
+                // The user has minimized the window.  Don't do anything in this case
+                return;
+            }
+
+            // Do we need to update things?
+            bool hasWindowChanged = (SpriteManager.Cameras[0].DestinationRectangle.Height != displayRectangle.Height) ||
+                (SpriteManager.Cameras[0].DestinationRectangle.Width != displayRectangle.Width);
+
+            if (hasWindowChanged)
+            {
+                // Resize the destination rectangle so the camera renders to the full screen
+                // You may need to change this code if using a split screen view.
+                SpriteManager.Cameras[0].DestinationRectangle = new Microsoft.Xna.Framework.Rectangle(
+                    0, 0, displayRectangle.Width, displayRectangle.Height);
+
+                #region Fix the Orthogonal values
+
+                double unitPerPixel = SpriteManager.Camera.OrthogonalHeight /
+                    SpriteManager.Cameras[0].DestinationRectangle.Height;
+
+                SpriteManager.Camera.OrthogonalHeight = (float)(displayRectangle.Height * unitPerPixel);
+                SpriteManager.Camera.OrthogonalWidth = (float)(displayRectangle.Width * unitPerPixel);
+
+                #endregion
+
+                #region Fix the 3D (FieldOfView and AspectRatio) values
+
+                // These values represent the field of view at 600 pixels.
+                // Increase the values (decrease the number that PI is divided by) to
+                // make the view wider (and make things appear smaller)
+                double yAt600 = Math.Sin(Math.PI / 8.0);
+                double xAt600 = Math.Cos(Math.PI / 8.0);
+                double desiredYAt600 = yAt600 * (double)displayRectangle.Height / 600.0;
+                float desiredAngle = (float)Math.Atan2(desiredYAt600, xAt600);
+                SpriteManager.Cameras[0].FieldOfView = 2 * desiredAngle;
+
+                SpriteManager.Cameras[0].FixAspectRatioYConstant();
+
+                #endregion
+            }
+        }
+
+
     }
 }
--- WinterEngine.Game/Screens/ToolsetScreen.cs
+++ WinterEngine.Game/Screens/ToolsetScreen.cs
@@ -51,8 +51,6 @@ namespace WinterEngine.Game.Screens
 
 		private void CustomDestroy()
 		{
-            FlatRedBallServices.CornerGrabbingResize -= ReactToResizing;
-
 		}
 
         private static void CustomLoadStaticContent(string contentManagerName)
@@ -68,7 +66,6 @@ namespace WinterEngine.Game.Screens
         private void BindEvents()
         {
             ToolsetUIEntityInstance.OnChangeScreen += base.ChangeScreen;
-            FlatRedBallServices.CornerGrabbingResize += ReactToResizing;
             
             // Area Editor
             ToolsetUIEntityInstance.OnAreaLoaded += OpenArea;
@@ -77,61 +74,6 @@ namespace WinterEngine.Game.Screens
             ToolsetUIEntityInstance.OnTilesetSpritesheetLoaded += TilesetEditorEntityInstance.LoadTilesetSpritesheet;
             TilesetEditorEntityInstance.OnTileSelected += ToolsetUIEntityInstance.LoadTile;
             ToolsetUIEntityInstance.OnTilesetEditorOpened += TilesetEditorEntityInstance.LoadTilesetEditor;
-        }
-
-        #endregion
-
-        #region Window resizing
-
-        private void ReactToResizing(object sender, EventArgs e)
-        {
-            // Get the new client bounds (the area where things will be drawn)
-            Microsoft.Xna.Framework.Rectangle displayRectangle =
-                FlatRedBallServices.Game.Window.ClientBounds;
-
-            // This tests if the user has minimized the window
-            if (displayRectangle.Width == 0 || displayRectangle.Height == 0)
-            {
-                // The user has minimized the window.  Don't do anything in this case
-                return;
-            }
-
-            // Do we need to update things?
-            bool hasWindowChanged = (SpriteManager.Cameras[0].DestinationRectangle.Height != displayRectangle.Height) || 
-                (SpriteManager.Cameras[0].DestinationRectangle.Width != displayRectangle.Width);
-
-            if (hasWindowChanged)
-            {
-                // Resize the destination rectangle so the camera renders to the full screen
-                // You may need to change this code if using a split screen view.
-                SpriteManager.Cameras[0].DestinationRectangle = new Microsoft.Xna.Framework.Rectangle(
-                    0, 0, displayRectangle.Width, displayRectangle.Height);
-
-                #region Fix the Orthogonal values
-
-                double unitPerPixel = SpriteManager.Camera.OrthogonalHeight /
-                    SpriteManager.Cameras[0].DestinationRectangle.Height;
-
-                SpriteManager.Camera.OrthogonalHeight = (float)(displayRectangle.Height * unitPerPixel);
-                SpriteManager.Camera.OrthogonalWidth = (float)(displayRectangle.Width * unitPerPixel);
-
-                #endregion
-
-                #region Fix the 3D (FieldOfView and AspectRatio) values
-
-                // These values represent the field of view at 600 pixels.
-                // Increase the values (decrease the number that PI is divided by) to
-                // make the view wider (and make things appear smaller)
-                double yAt600 = Math.Sin(Math.PI / 8.0);
-                double xAt600 = Math.Cos(Math.PI / 8.0);
-                double desiredYAt600 = yAt600 * (double)displayRectangle.Height / 600.0;
-                float desiredAngle = (float)Math.Atan2(desiredYAt600, xAt600);
-                SpriteManager.Cameras[0].FieldOfView = 2 * desiredAngle;
-
-                SpriteManager.Cameras[0].FixAspectRatioYConstant();
-
-                #endregion
-            }
         }
 
         #endregion
