e641023a35f2f349636ef5d094ceb744e65ce882
zunath <coolty3001@yahoo.com>
2013-11-24 19:11:36 -0600

    Bootstrap conversion round 3

--- WinterEngine.UI/Scripts/Toolset.Initialization.js
+++ WinterEngine.UI/Scripts/Toolset.Initialization.js
@@ -4,9 +4,6 @@ function Initialize() {
     InitializeToolsetViewModel(); // Located in WinterEngine.ViewModels.js
     InitializeValidation();
     
-    $('.clsProgressBar').progressbar({
-        value: false
-    });
 
     // Unblock the UI - the UI blocking is done to prevent the user from making javascript calls before Awesomium has loaded.
     $.unblockUI();
